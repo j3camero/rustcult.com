788068dabe051976967f8174237d617c834a354a
Ubuntu <ubuntu@ip-172-31-9-14.us-west-2.compute.internal>
2023-01-19 00:56:10 +0000

    Add webserver

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = 8080;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'static' });
+});
+
+app.use(express.static('static'));
+
+app.listen(port, () => console.log(`Server listening on port: ${PORT}`));
